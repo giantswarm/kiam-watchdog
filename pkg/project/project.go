682ff8f53d366cbd75efd22899ff849353dc9771
GitHub Action <action@github.com>
2021-07-29 12:16:44 +0000

    Bump version to 0.1.2-dev

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -5,7 +5,7 @@ var (
 	gitSHA      = "n/a"
 	name        = "kiam-watchdog"
 	source      = "https://github.com/giantswarm/kiam-watchdog"
-	version     = "0.1.1"
+	version     = "0.1.2-dev"
 )
 
 func Description() string {
