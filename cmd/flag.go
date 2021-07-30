4e8acff1e17509e9f7e5fb40d05eb8f8d7a465d4
Christian Bianchi <whites11@gmail.com>
2021-07-30 09:29:20 +0200

    Remove default value for AWS region.

--- cmd/flag.go
+++ cmd/flag.go
@@ -32,7 +32,7 @@ func (f *flag) Init(cmd *cobra.Command) {
 	// Add command line flags for glog.
 	pflag.CommandLine.AddGoFlagSet(goflag.CommandLine)
 
-	cmd.Flags().StringVar(&f.Region, regionFlag, "eu-west-1", `The AWS region to use for the tests. Defaults to eu-west-1.`)
+	cmd.Flags().StringVar(&f.Region, regionFlag, "", `The AWS region to use for the tests.`)
 	cmd.Flags().IntVar(&f.FailThreshold, failThresholdFlag, 5, `How many failed probes in a row to consider kiam unhealthy. Defaults to 5.`)
 	cmd.Flags().IntVar(&f.Interval, intervalFlag, 60, `Interval in seconds to wait between tests. Defaults to 60.`)
 
@@ -43,6 +43,22 @@ func (f *flag) Init(cmd *cobra.Command) {
 
 func (f *flag) Validate(cmd *cobra.Command) error {
 	var err error
+
+	if f.Region == "" {
+		return fmt.Errorf("--%s can't be empty", regionFlag)
+	}
+
+	if f.KiamNamespace == "" {
+		return fmt.Errorf("--%s can't be empty", kiamNamespaceFlag)
+	}
+
+	if f.KiamLabelSelector == "" {
+		return fmt.Errorf("--%s can't be empty", kiamLabelSelectorFlag)
+	}
+
+	if f.NodeName == "" {
+		return fmt.Errorf("--%s can't be empty", nodeNameFlag)
+	}
 
 	if f.Interval <= 0 {
 		return fmt.Errorf("--%s should be greater than 0", intervalFlag)
